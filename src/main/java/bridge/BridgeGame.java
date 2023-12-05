367d18941c64e8879800d4730004a84cb56ce11b
zangsu <zangsu_@naver.com>
2023-12-05 14:05:47 +0900

    feat : 흐름 구성 스켈레톤 코드 작성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,23 @@
 package bridge;
 
+import bridge.domain.GameState;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
+ *  메서드의 이름은 변경할 수 없고, 인자와 반환 타입은 필요에 따라 추가하거나 변경할 수 있다.
  */
 public class BridgeGame {
+
+    public void run(){
+        /*GameState gameState = new GameState();
+        while(!gameState.isFinished()){
+            boolean moveSuccess = move(gameState);
+            if(!moveSuccess){
+                retry(gameState);
+            }
+        }
+        outputView.printResult(gameState);*/
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -11,6 +25,8 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+        //사용자 입력 받는다.
+        /*return gameState.move(입력);*/
     }
 
     /**
@@ -19,5 +35,9 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        //사용자 입력 받는다.
+        /*if(재시도){
+            gameState.retry();
+        }*/
     }
 }
---
+++ src/main/java/bridge/domain/GameState.java
@@ -0,0 +1,5 @@
+package bridge.domain;
+
+public class GameState {
+
+}
