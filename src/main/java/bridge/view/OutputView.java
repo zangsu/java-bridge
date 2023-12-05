2777b17f82419f268a5644d0a3b725101c18574d
zangsu <zangsu_@naver.com>
2023-12-05 13:33:31 +0900

    feat(View) : view 패키지 이동 및 에러 메시지 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,9 +1,11 @@
-package bridge;
+package bridge.view;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    public static final String ERROR_PREFIX = "[ERROR] ";
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -19,5 +21,13 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void printException(IllegalArgumentException e){
+        System.out.println(ERROR_PREFIX + e.getMessage());
+    }
+
+    public void newLine() {
+        System.out.println();
     }
 }
