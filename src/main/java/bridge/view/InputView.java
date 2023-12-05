5edfde9389f59ada88101520577729060008d95d
zangsu <zangsu_@naver.com>
2023-12-05 13:47:06 +0900

    feat : 입력 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,66 @@
 package bridge.view;
 
+import bridge.exception.BridgeException;
+import bridge.exception.handler.RetryHandler;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    public static final Pattern MOVING_INPUT_PATTERN = Pattern.compile("[UD]");
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        return RetryHandler.getOrRetry(this::getInteger);
+    }
+
+    private int getInteger() {
+        String input = Console.readLine();
+        try{
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e){
+            throw BridgeException.INVALID_NUMBER_FORMAT.makeException();
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        return RetryHandler.getOrRetry(this::getMovingInput);
+    }
+
+    private String getMovingInput() {
+        String input = Console.readLine();
+        if(MOVING_INPUT_PATTERN.matcher(input).matches()){
+            return input;
+        }
+        throw BridgeException.INVALID_INPUT_FORMAT.makeException();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public boolean askRestart() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        return RetryHandler.getOrRetry(() -> getBoolean("R", "Q"));
+    }
+
+    private boolean getBoolean(String trueInput, String falseInput){
+        String input = Console.readLine();
+        if(trueInput.equals(input)){
+            return true;
+        }
+        if(falseInput.equals(input)){
+            return false;
+        }
+        throw BridgeException.INVALID_INPUT_FORMAT.makeException();
     }
 }
