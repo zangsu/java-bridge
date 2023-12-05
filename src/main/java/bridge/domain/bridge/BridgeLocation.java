a5e642bb41e4662ad1ca3e74d057d66f37346ae7
zangsu <zangsu_@naver.com>
2023-12-05 16:32:55 +0900

    refactor : enum 에서 사용하지 않는 int 값 삭제

--- src/main/java/bridge/domain/bridge/BridgeLocation.java
+++ src/main/java/bridge/domain/bridge/BridgeLocation.java
@@ -3,18 +3,15 @@ package bridge.domain.bridge;
 import bridge.exception.BridgeException;
 import java.util.Arrays;
 
-// todo 의미 없는 enum 같기도 해서 고민중
 public enum BridgeLocation {
-    UP("U", 1),
-    DOWN("D", 0),
+    UP("U"),
+    DOWN("D"),
     ;
 
     private final String inputMessage;
-    private final int inputNumber;
 
-    BridgeLocation(String inputMessage, int inputNumber) {
+    BridgeLocation(String inputMessage) {
         this.inputMessage = inputMessage;
-        this.inputNumber = inputNumber;
     }
 
     public static BridgeLocation from(String inputMessage) {
@@ -22,13 +19,6 @@ public enum BridgeLocation {
                 .filter(location -> location.inputMessage.equals(inputMessage))
                 .findFirst()
                 .orElseThrow(BridgeException.INVALID_INPUT_FORMAT::makeException);
-    }
-
-    public static BridgeLocation from(int generatedNumber) {
-        return Arrays.stream(values())
-                .filter(location -> location.inputNumber == generatedNumber)
-                .findFirst()
-                .orElseThrow(BridgeException.INVALID_BRIDGE_SHAPE::makeException);
     }
 
     public String getMessage() {
